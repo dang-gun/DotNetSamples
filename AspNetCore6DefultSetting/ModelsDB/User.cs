9face99d4383f5217760b57707125d6ffbc28b11
dang-gun <c1dong8@gmail.com>
2022-03-29 13:11:10 +0900

    ASP.NET Core 6 기본 프로젝트 세팅 완료

---
+++ AspNetCore6DefultSetting/ModelsDB/User.cs
@@ -0,0 +1,19 @@
+﻿namespace ModelsDB
+{
+	public class User
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Id { get; set; }
+		
+		/// <summary>
+		/// Sign-in Name
+		/// </summary>
+		public string SignName { get; set; } = string.Empty;
+		/// <summary>
+		/// Sign-in Password
+		/// </summary>
+		public string Password { get; set; } = string.Empty;
+	}
+}
---
+++ AspNetCore6DefultSetting/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace AspNetCore6DefultSetting
+{
+	public class Startup
+	{
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //API모델을 파스칼 케이스 유지하기
+            //2.0
+            //services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            //3.0
+            services.AddControllers().AddNewtonsoftJson(options => { options.SerializerSettings.ContractResolver = new DefaultContractResolver(); });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                //스웨거 사용
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+            }
+
+            //스웨거 사용
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            //3.0 api 라우트
+            app.UseRouting();
+
+            //기본 페이지
+            app.UseDefaultFiles();
+            //wwwroot
+            app.UseStaticFiles();
+
+
+            //3.0 api 라우트 끝점
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
