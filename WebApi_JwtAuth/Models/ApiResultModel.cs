976c895a65d25bf15226d645f379823ddf6f0aa4
dang-gun <c1dong8@gmail.com>
2022-04-01 13:50:55 +0900

    WebApi_JwtAuth 프로젝트 추가

---
+++ WebApi_JwtAuth/Models/ApiResultModel.cs
@@ -0,0 +1,17 @@
+﻿using ModelsDB;
+
+namespace WebApi_JwtAuth.Models
+{
+	public class ApiResultModel
+    {
+        /// <summary>
+        /// 성공여부
+        /// </summary>
+        public bool Complete { get; set; } = false;
+
+        /// <summary>
+        /// 메시지
+        /// </summary>
+        public string Message { get; set; } = string.Empty;
+    }
+}
