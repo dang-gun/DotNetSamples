aa2604f013976380e2f68a8b70375fb0a65b83b9
dang-gun <c1dong8@gmail.com>
2022-06-08 19:05:20 +0900

    스톱워치 예제 추가

--- StopwatchTimerTest/Form1.Designer.cs
+++ StopwatchTimerTest/Form1.Designer.cs
@@ -28,33 +28,86 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.labTimer = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.labStopwatch = new System.Windows.Forms.Label();
+			this.btn10Sec = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// button1
+			// groupBox1
 			// 
-			this.button1.Location = new System.Drawing.Point(62, 37);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.groupBox1.Controls.Add(this.labTimer);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(200, 100);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Timer";
+			// 
+			// labTimer
+			// 
+			this.labTimer.AutoSize = true;
+			this.labTimer.Location = new System.Drawing.Point(6, 20);
+			this.labTimer.Name = "labTimer";
+			this.labTimer.Size = new System.Drawing.Size(39, 15);
+			this.labTimer.TabIndex = 0;
+			this.labTimer.Text = "label1";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.labStopwatch);
+			this.groupBox2.Location = new System.Drawing.Point(244, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(200, 100);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Stopwatch";
+			// 
+			// labStopwatch
+			// 
+			this.labStopwatch.AutoSize = true;
+			this.labStopwatch.Location = new System.Drawing.Point(6, 20);
+			this.labStopwatch.Name = "labStopwatch";
+			this.labStopwatch.Size = new System.Drawing.Size(39, 15);
+			this.labStopwatch.TabIndex = 0;
+			this.labStopwatch.Text = "label2";
+			// 
+			// btn10Sec
+			// 
+			this.btn10Sec.Location = new System.Drawing.Point(12, 145);
+			this.btn10Sec.Name = "btn10Sec";
+			this.btn10Sec.Size = new System.Drawing.Size(157, 23);
+			this.btn10Sec.TabIndex = 3;
+			this.btn10Sec.Text = "10초 진행";
+			this.btn10Sec.UseVisualStyleBackColor = true;
+			this.btn10Sec.Click += new System.EventHandler(this.btn10Sec_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(479, 261);
+			this.Controls.Add(this.btn10Sec);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private Button button1;
+		private GroupBox groupBox1;
+		private GroupBox groupBox2;
+		private Button btn10Sec;
+		private Label labTimer;
+		private Label labStopwatch;
 	}
 }
--- StopwatchTimerTest/Form1.cs
+++ StopwatchTimerTest/Form1.cs
@@ -2,32 +2,96 @@ namespace StopwatchTimerTest
 {
 	public partial class Form1 : Form
 	{
+		
+
+		/// <summary>
+		/// 10초동작 타이머
+		/// </summary>
+		private System.Timers.Timer _timer10Sec;
+
+		/// <summary>
+		/// 0.1초 동작 타이머
+		/// </summary>
+		private System.Timers.Timer _timer01Sec;
+		/// <summary>
+		/// 스톱워치 타이머
+		/// </summary>
 		private StopwatchTimer.Timer stopwatchTimer;
 
-		private int nCount = 0;
+		/// <summary>
+		/// 진행중 여부
+		/// </summary>
+		private bool _bProceeding = false;
 
 		public Form1()
 		{
 			InitializeComponent();
 
+			this._timer10Sec = new System.Timers.Timer();
+			this._timer10Sec.Interval = 10 * 1000;
+			this._timer10Sec.Elapsed += _timer10Sec_Elapsed;
+
+			this._timer01Sec = new System.Timers.Timer();
+			this._timer01Sec.Interval = 10;
+			this._timer01Sec.Elapsed += _timer01Sec_Elapsed;
+
 			this.stopwatchTimer = new StopwatchTimer.Timer();
-			this.stopwatchTimer.Interval = 1000;
+			this.stopwatchTimer.Interval = 10;
 			this.stopwatchTimer.OnElapsed += StopwatchTimer_OnElapsed;
 		}
 
-		private async void button1_Click(object sender, EventArgs e)
+		
+
+		private void _timer10Sec_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
 		{
-			await this.stopwatchTimer.Start();
+			//다른타이머 모두 정지
+			this._timer01Sec.Stop();
+			this.stopwatchTimer.Stop();
+
+			this.btn10Sec.Enabled = true;
+		}
+
+		/// <summary>
+		/// 0.1초 타이머 카운트
+		/// </summary>
+		private int _nTimer01Sec_Count = 0;
+		private void _timer01Sec_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+		{
+			this.Invoke(new Action(delegate ()
+			{
+				++this._nTimer01Sec_Count;
+				this.labTimer.Text = this._nTimer01Sec_Count.ToString();
+			}));
+			
+		}
+
+		/// <summary>
+		/// 스톱워치 타이머 카운트
+		/// </summary>
+		private int _nStopwatchTimer_Count = 0;
+		private void StopwatchTimer_OnElapsed(object sender)
+		{
+			
+			this.Invoke(new Action(delegate ()
+			{
+				++this._nStopwatchTimer_Count;
+				this.labStopwatch.Text = this._nStopwatchTimer_Count.ToString();
+			}));
 
 		}
 
-		private void StopwatchTimer_OnElapsed(object sender)
-		{
-			this.Invoke(new Action(delegate () 
-			{
-				this.Text = (++nCount).ToString();
-			}));  
 
+		private async void btn10Sec_Click(object sender, EventArgs e)
+		{
+			this.btn10Sec.Enabled = false;
+
+			this._nTimer01Sec_Count = 0;
+			this._nStopwatchTimer_Count = 0;
+
+			//타이머 모두 시작
+			this._timer10Sec.Start();
+			this._timer01Sec.Start();
+			await this.stopwatchTimer.Start();
 		}
 	}
 }
