0805fc95688a1ca0e4ae0ff9b6604ad8e3a8e6ac
dang-gun <c1dong8@gmail.com>
2022-07-20 18:34:54 +0900

    .NET 6 스타일로 변경

--- AspNetCore6DefultSetting/Program.cs
+++ AspNetCore6DefultSetting/Program.cs
@@ -1,17 +1,39 @@
-using Microsoft.AspNetCore;
+using Newtonsoft.Json.Serialization;
 
-namespace AspNetCore6DefultSetting
-{
+var builder = WebApplication.CreateBuilder(args);
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+#region ConfigureServices
+//API모델을 파스칼 케이스 유지하기
+builder.Services.AddControllers().AddNewtonsoftJson(options => { options.SerializerSettings.ContractResolver = new DefaultContractResolver(); });
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
+
+#region Configure
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{//개발 버전에서만 스웨거 사용
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+//3.0 api 라우트
+app.UseRouting();
+//https로 자동 리디렉션
+app.UseHttpsRedirection();
+
+//기본 페이지
+app.UseDefaultFiles();
+//wwwroot
+app.UseStaticFiles();
+
+
+app.MapControllers();
+
+app.Run();
+#endregion
