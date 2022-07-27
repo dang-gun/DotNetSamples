a7d9648e7aac317d6fae743942b8bef46ab7fc84
dang-gun <c1dong8@gmail.com>
2022-07-27 17:46:49 +0900

    취약성 검사 또또다시

--- ReactTest/Controllers/TestController.cs
+++ ReactTest/Controllers/TestController.cs
@@ -1,23 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace ReactTest.Controllers
+namespace ReactTest.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class TestController : ControllerBase
 {
-	[Route("api/[controller]/[action]")]
-	[ApiController]
-	public class TestController : ControllerBase
-	{
-        /// <summary>
-        /// 무조건 성공
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public ActionResult SuccessCall()
-        {
-            ObjectResult apiresult = new ObjectResult(200);
+    /// <summary>
+    /// 무조건 성공
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public ActionResult SuccessCall()
+    {
+        ObjectResult apiresult = new ObjectResult(200);
 
-            apiresult = StatusCode(200, "성공!");
+        apiresult = StatusCode(200, "성공!");
 
-            return apiresult;
-        }
+        return apiresult;
     }
 }
