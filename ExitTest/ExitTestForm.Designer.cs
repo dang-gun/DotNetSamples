82e324ee1680d13d9f7d2f54183e55c5e95eb463
dang-gun <c1dong8@gmail.com>
2022-08-23 11:12:27 +0900

    ExitTest : 프로젝트 생성

---
+++ ExitTest/ExitTestForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ExitTest
+{
+	partial class ExitTestForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnApplicationExit = new System.Windows.Forms.Button();
+			this.btnEnvironmentExit = new System.Windows.Forms.Button();
+			this.btnProcessKill = new System.Windows.Forms.Button();
+			this.btnForegroundEnd = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnApplicationExit
+			// 
+			this.btnApplicationExit.Location = new System.Drawing.Point(12, 12);
+			this.btnApplicationExit.Name = "btnApplicationExit";
+			this.btnApplicationExit.Size = new System.Drawing.Size(207, 23);
+			this.btnApplicationExit.TabIndex = 0;
+			this.btnApplicationExit.Text = "Application.Exit()";
+			this.btnApplicationExit.UseVisualStyleBackColor = true;
+			this.btnApplicationExit.Click += new System.EventHandler(this.btnApplicationExit_Click);
+			// 
+			// btnEnvironmentExit
+			// 
+			this.btnEnvironmentExit.Location = new System.Drawing.Point(12, 70);
+			this.btnEnvironmentExit.Name = "btnEnvironmentExit";
+			this.btnEnvironmentExit.Size = new System.Drawing.Size(207, 23);
+			this.btnEnvironmentExit.TabIndex = 1;
+			this.btnEnvironmentExit.Text = "Environment.Exit()";
+			this.btnEnvironmentExit.UseVisualStyleBackColor = true;
+			this.btnEnvironmentExit.Click += new System.EventHandler(this.btnEnvironmentExit_Click);
+			// 
+			// btnProcessKill
+			// 
+			this.btnProcessKill.Location = new System.Drawing.Point(12, 99);
+			this.btnProcessKill.Name = "btnProcessKill";
+			this.btnProcessKill.Size = new System.Drawing.Size(207, 23);
+			this.btnProcessKill.TabIndex = 2;
+			this.btnProcessKill.Text = "Process.GetCurrentProcess().Kill();";
+			this.btnProcessKill.UseVisualStyleBackColor = true;
+			this.btnProcessKill.Click += new System.EventHandler(this.btnProcessKill_Click);
+			// 
+			// btnForegroundEnd
+			// 
+			this.btnForegroundEnd.Location = new System.Drawing.Point(109, 41);
+			this.btnForegroundEnd.Name = "btnForegroundEnd";
+			this.btnForegroundEnd.Size = new System.Drawing.Size(110, 23);
+			this.btnForegroundEnd.TabIndex = 3;
+			this.btnForegroundEnd.Text = "Foreground End";
+			this.btnForegroundEnd.UseVisualStyleBackColor = true;
+			this.btnForegroundEnd.Click += new System.EventHandler(this.btnForegroundEnd_Click);
+			// 
+			// ExitTestForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(233, 147);
+			this.Controls.Add(this.btnForegroundEnd);
+			this.Controls.Add(this.btnProcessKill);
+			this.Controls.Add(this.btnEnvironmentExit);
+			this.Controls.Add(this.btnApplicationExit);
+			this.Name = "ExitTestForm";
+			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ExitTestForm_FormClosed);
+			this.Load += new System.EventHandler(this.ExitTestForm_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Button btnApplicationExit;
+		private Button btnEnvironmentExit;
+		private Button btnProcessKill;
+		private Button btnForegroundEnd;
+	}
+}
---
+++ ExitTest/ExitTestForm.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+
+namespace ExitTest
+{
+	public partial class ExitTestForm : Form
+	{
+		/// <summary>
+		/// 포그라운드 쓰래드 재생여부
+		/// </summary>
+		private bool _bForeground = true;
+		
+		public ExitTestForm()
+		{
+			InitializeComponent();
+		}
+
+		private void ExitTestForm_Load(object sender, EventArgs e)
+		{
+			Thread threadForeground 
+				= new Thread(()=> 
+				{
+					while (true == this._bForeground)
+					{
+						Thread.Sleep(1000);
+						Debug.WriteLine(String.Format("Foreground :{0}", DateTime.Now.ToString("HH:mm:ss")));
+					};
+				});
+
+			threadForeground.Start();
+		}
+
+		private void ExitTestForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Debug.WriteLine(String.Format("FormClosed :{0}", DateTime.Now.ToString("HH:mm:ss")));
+		}
+
+		private void btnForegroundEnd_Click(object sender, EventArgs e)
+		{
+			this._bForeground = false;
+		}
+
+		private void btnApplicationExit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void btnEnvironmentExit_Click(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		private void btnProcessKill_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.GetCurrentProcess().Kill();
+		}
+
+		
+	}
+}
