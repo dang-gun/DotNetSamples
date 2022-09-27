651e65143b4842c4db0bc1570a3b986c408f418e
dang-gun <c1dong8@gmail.com>
2022-09-27 19:06:27 +0900

    업로드 금시 설정파일 추가

--- EfMultiMigrations_Asp/Program.cs
+++ EfMultiMigrations_Asp/Program.cs
@@ -1,8 +1,15 @@
 using EfMultiMigrations;
 using EfMultiMigrations.Models;
+using EfMultiMigrations_Asp;
+using Newtonsoft.Json;
 using System.Data;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//설정 파일 읽기
+string sJson = File.ReadAllText("SettingInfo_gitignore.json");
+SettingInfoModel? loadGBI = JsonConvert.DeserializeObject<SettingInfoModel>(sJson);
 
 // Add services to the container.
 ModelDllGlobal.DbType = TargetDbType.Sqlite;
@@ -13,8 +20,9 @@ switch (ModelDllGlobal.DbType)
 	case TargetDbType.Mssql:
 		//Add-Migration InitialCreate -Context ModelsDB.DbModel_MssqlContext -OutputDir Migrations/Mssql
 		//Update-Database -Context ModelsDB.DbModel_MssqlContext -Migration 0	
-		ModelDllGlobal.DbConnectString
-			= "Server=[주소];DataBase=[데이터 베이스];UId=[아이디];pwd=[비밀번호]";
+		//ModelDllGlobal.DbConnectString
+		//	= "Server=[주소];DataBase=[데이터 베이스];UId=[아이디];pwd=[비밀번호]";
+		ModelDllGlobal.DbConnectString = loadGBI!.ConnectionString_Mssql;
 		break;
 
 	case TargetDbType.Sqlite:
