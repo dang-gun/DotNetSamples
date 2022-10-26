c8d28aa9ba672b72f5cebeea43d1c3ce8f9802e3
dang-gun <c1dong8@gmail.com>
2022-10-26 11:43:37 +0900

    [ASP.NET Core] URL 재작성(Rewrite)을 이용한 라우팅(Routing)

--- ASPNET_RewriteRouting/Startup.cs
+++ ASPNET_RewriteRouting/Startup.cs
@@ -42,22 +42,19 @@ public class Startup
 
 
 		//경로 지정
-		//var rewrite = new RewriteOptions()
-		//	.AddRewrite("home/(.*)", "/home1/$1", true)
-		//	.AddRewrite("home", "/home1/", true)
+		RewriteOptions rewrite = new RewriteOptions()
+			.AddRewrite("home/(.*)", "/home1/$1", true)
+			.AddRewrite("home", "/home1/", true)
 
-		//	.AddRewrite("admin/(.*)", "/admin1/$1", true)
-		//	.AddRewrite("admin", "/admin1/", true)
+			.AddRewrite("admin/(.*)", "/admin1/$1", true)
+			.AddRewrite("admin", "/admin1/", true)
 
-		//	.AddRewrite("long/(.*)", "/long1/$1", true)
-		//	.AddRewrite("long", "/long1/", true)
-
-		//	//.AddRewrite("(.*)", "/production-home/$1", true)
-		//	;
-		//app.UseRewriter(rewrite);
+			.AddRewrite("log/(.*)", "/log1/$1", true)
+			.AddRewrite("log", "/log1/", true);
+		app.UseRewriter(rewrite);
 
 
-		
+
 		//3.0 api 라우트
 		app.UseRouting();
 		//https로 자동 리디렉션
@@ -67,81 +64,13 @@ public class Startup
 
 		//기본 페이지
 		app.UseDefaultFiles();
-
-
 		//wwwroot
-		//app.UseStaticFiles();
-		app
-		.UseStaticFiles()
-		//.UseStaticFiles(new StaticFileOptions()
-		//{
-		//	FileProvider = new PhysicalFileProvider(
-		//		Path.Combine(env.ContentRootPath
-		//						, @"wwwroot\Home1")),
-		//	RequestPath = new PathString("/home"),
-		//})
-		//.UseStaticFiles(new StaticFileOptions()
-		//{
-		//	FileProvider = new PhysicalFileProvider(
-		//		Path.Combine(env.ContentRootPath
-		//						, @"wwwroot\production-admin")),
-		//	RequestPath = new PathString("/admin"),
-		//})
-		//.UseStaticFiles(new StaticFileOptions()
-		//{
-		//	FileProvider = new PhysicalFileProvider(
-		//		Path.Combine(env.ContentRootPath
-		//				, @"wwwroot\aaa")),
-		//	RequestPath = new PathString("/aaa"),
-		//})
-		;
-
-
-		app.Map("/home",
-		   HomeApp =>
-		   {
-			   //3.0 api 라우트
-			   HomeApp.UseRouting();
-			   //https로 자동 리디렉션
-			   HomeApp.UseHttpsRedirection();
-
-			   // var rewrite = new RewriteOptions()
-			   //.AddRewrite("(.*)", "/$1", true);
-			   // HomeApp.UseRewriter(rewrite);
-
-			   HomeApp.UseDefaultFiles();
-
-			   // //DefaultFilesOptions options = new DefaultFilesOptions();
-			   // //options.DefaultFileNames.Clear();
-			   // //options.DefaultFileNames.Add("/home1/index.html");
-			   // //HomeApp.UseDefaultFiles(options);
-
-			   HomeApp.UseStaticFiles(new StaticFileOptions()
-			   {
-				   FileProvider = new PhysicalFileProvider(
-				  Path.Combine(env.ContentRootPath
-						  , @"wwwroot\Home1")),
-				   //RequestPath = new PathString("/home"),
-			   });
-
-			   //HomeApp.Run(async context => {
-			   // Console.WriteLine("I am your life handler");
-			   // await context.Response.WriteAsync("I am your life handler");
-			   //});
-
-			   HomeApp.UseEndpoints(endpoints =>
-			   {
-				   endpoints.MapFallbackToFile("/index.html");
-			   });
-		   });
-
-
+		app.UseStaticFiles();
+		
 
 		app.UseEndpoints(endpoints =>
 		{
-			
 			endpoints.MapControllers();
-			endpoints.MapFallbackToFile("/index.html");
 		});
 	}
 }
